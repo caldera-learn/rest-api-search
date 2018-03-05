16bc8e8bbb75c27d28d078b8fdd2f3dde5341288
Josh Pollock <josh@calderawp.com>
2018-03-05 14:52:02 -0500

    finish tests for FilterWPQuery

--- Tests/Mock/WP_Post.php
+++ Tests/Mock/WP_Post.php
@@ -1,27 +1,26 @@
 <?php
 
 // phpcs:disable
-if( ! class_exists( '\WP_Post' ) ){
-    /**
-     * Class WP_Post
-     *
-     * Mock for WP_Post
-     *
-     * @package CalderaLearn\RestSearch\Mock
-     */
-    class WP_Post extends \stdClass
-    {
-        /** @var string */
-        public $post_title;
+if (! class_exists('\WP_Post')) {
+	/**
+	 * Class WP_Post
+	 *
+	 * Mock for WP_Post
+	 *
+	 * @package CalderaLearn\RestSearch\Mock
+	 */
+	class WP_Post extends \stdClass
+	{
+		/** @var string */
+		public $post_title;
 
-        /**
-         * WP_Post constructor.
-         * @param mixed $post Could be anything, not used
-         */
-        public function __construct($post)
-        {
-            //Don't do anything
-        }
-    }
+		/**
+		 * WP_Post constructor.
+		 * @param mixed $post Could be anything, not used
+		 */
+		public function __construct($post)
+		{
+			//Don't do anything
+		}
+	}
 }
-
--- Tests/Unit/FilterWPQueryTest.php
+++ Tests/Unit/FilterWPQueryTest.php
@@ -4,7 +4,6 @@
 namespace CalderaLearn\RestSearch\Tests\Unit;
 
 use CalderaLearn\RestSearch\Tests\Mock\FilterWPQuery;
-use CalderaLearn\RestSearch\Tests\Mock\WP_Post;
 
 class FilterWPQueryTest extends TestCase
 {
@@ -31,24 +30,35 @@ class FilterWPQueryTest extends TestCase
 	{
 		//Get the mock posts
 		$results = FilterWPQuery::getPosts();
-		$this->assertFalse( empty( $results ) );
+		$this->assertFalse(empty($results));
 		//Make sure results are an array of WP_Posts
 		$looped = false;
 		foreach ($results as $result) {
 			$looped = true;
 			//Make sure all results are WP_Posts
-			$this->assertTrue(is_a($result, '\WP_Post'), get_class( $result ));
+			$this->assertTrue(is_a($result, '\WP_Post'), get_class($result));
 		}
 		//Make sure loop ran
 		$this->assertTrue($looped);
 	}
 
 	/**
+	 * Test that our mock returns true for should filter
+	 *
+	 * @covers FilterWPQuery::shouldFilter()
+	 * @covers FilterWPQueryTest::testWithNull()
+	 */
+	public function testShouldFilter()
+	{
+		$this->assertTrue(FilterWPQuery::shouldFilter());
+	}
+
+	/**
 	 * Test the result data is consistent
 	 *
-	 * @covers FilterWPQuery::callback()
+	 * * @covers \CalderaLearn\RestSearch\FilterWPQuery::callback()
 	 */
-	public function testWithNull()
+	public function testCallbackWithNull()
 	{
 		//Use the mock data we have in our mock class as the expected values
 		$expected = FilterWPQuery::getPosts();
@@ -69,7 +79,46 @@ class FilterWPQueryTest extends TestCase
 		/**
 		 * Loop through expected, comparing to eactual results
 		 * @var int $i
-		 * @var  WP_Post $post
+		 * @var  \WP_Post $post
+		 */
+		foreach ($expected as $i => $post) {
+			$looped = true;
+			//Test that the mock and resulting post titles are the same
+			$this->assertSame($post->post_title, $results[$i]->post_title);
+		}
+		//Make sure loop ran
+		$this->assertTrue($looped);
+	}
+
+	/**
+	 * Test the result data is not changed, when passed an array
+	 *
+	 * * @covers \CalderaLearn\RestSearch\FilterWPQuery::callback()
+	 */
+	public function testCallbackWithArray()
+	{
+		//Create 1 mock posts
+		$post = new \WP_Post((new \stdClass()));
+		$post->post_title = 'The title of this post is post';
+		$expected = [ $post ];
+
+		//Get the results from the callback
+		$results  = FilterWPQuery::callback($expected);
+
+		//Make sure results are an array
+		$this->assertTrue(is_array($results));
+
+		//Make sure the two arrays are the same size
+		$this->assertCount(count($expected), $results);
+
+		/** These arrays are not the same, compare the meaning of the contents */
+
+		//Used to make sure this loop of tests ran
+		$looped = false;
+		/**
+		 * Loop through expected, comparing to eactual results
+		 * @var int $i
+		 * @var  \WP_Post $post
 		 */
 		foreach ($expected as $i => $post) {
 			$looped = true;
