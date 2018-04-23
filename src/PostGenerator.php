8573d1664c37982e8ddc9864dafc6462c3b94e03
hellofromtonya <hellofromtonya@knowthecode.io>
2018-04-23 12:36:02 -0500

    Removed the unnecessary array index.
    The posts start at index 0.  Therefore, there is no need to
    specifically specify the new post's position in the array.
    
    This change simplifies the code and makes it more readable.

--- src/PostGenerator.php
+++ src/PostGenerator.php
@@ -17,10 +17,10 @@ class PostGenerator
 	public static function generate($quantity) : array {
 		$mockPosts = [];
 		for ($postNumber = 0; $postNumber <= $quantity; $postNumber++) {
-			$post                     = new WP_Post( new stdClass() );
-			$post->post_title         = "Mock Post {$postNumber}";
-			$post->filter             = 'raw';
-			$mockPosts[ $postNumber ] = $post;
+			$post             = new WP_Post( new stdClass() );
+			$post->post_title = "Mock Post {$postNumber}";
+			$post->filter     = 'raw';
+			$mockPosts[]      = $post;
 		}
 
 		return $mockPosts;
