2c610273cafaf07c5e79853da300a3ca45f2cc5f
hellofromtonya <hellofromtonya@knowthecode.io>
2018-04-25 18:08:41 -0500

    Removed unnecessary parentheses from post instantiation.
    The keyword `new` creates an object for us.  The only time we
    need to wrap an object inside of a pair of parentheses is when
    we want to chain a method directly to the object without assigning
    the object to a variable.
    
    Here, removing the unnecessary () makes the code more readable.
    
    Remember, formatting matters. :)

--- src/FilterWPQuery.php
+++ src/FilterWPQuery.php
@@ -93,7 +93,7 @@ class FilterWPQuery implements FiltersPreWPQuery
 	{
 		$mockPosts = [];
 		for ($i = 0; $i < $quantity; $i++) {
-			$post = new \WP_Post((new \stdClass()));
+			$post = new \WP_Post( new \stdClass() );
 			$post->post_title = "Mock Post $i";
 			$post->filter = 'raw';
 			$mockPosts[$i] = $post;
