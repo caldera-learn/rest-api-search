2c2f728af260aa81a6f52ce209da4d20b8088fdd
hellofromtonya <hellofromtonya@knowthecode.io>
2018-04-25 08:58:20 -0500

    Replaced hard-coded priority with the property.
    The class defines the priority number.  It's redundant to
    hard-code the priority number in the `add_filter` and `remove_filter`
    tasks.  This change makes the code DRY.
    
    What are the advantages here?
    
    The priority number can change in one place, i.e. in the property.
    This is important as you may want to change the priority.  But you
    might forget to change it in each of the instances. Whoopsie.  That
    will create a bug.
    
    Using the new strategy, the priority number is set in only one place.
    Then the code gets that value when doing its work.

--- src/FilterWPQuery.php
+++ src/FilterWPQuery.php
@@ -62,13 +62,13 @@ class FilterWPQuery implements FiltersPreWPQuery
 	/** @inheritdoc */
 	public static function addFilter() : bool
 	{
-		return add_filter('posts_pre_query', [FilterWPQuery::class, 'filterPreQuery'], 10);
+		return add_filter('posts_pre_query', [FilterWPQuery::class, 'filterPreQuery'], static::$filterPriority);
 	}
 
 	/** @inheritdoc */
 	public static function removeFilter() : bool
 	{
-		return remove_filter('posts_pre_query', [FilterWPQuery::class, 'filterPreQuery'], 10);
+		return remove_filter('posts_pre_query', [FilterWPQuery::class, 'filterPreQuery'], static::$filterPriority);
 	}
 
 	/** @inheritdoc */
