aa7158930e67624c268164ed1abc1b7836c29514
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-11 17:10:35 -0500

    Wired the generic object to getPosts.
    Notice how getPosts() uses the new generic $contentGetter object
    to invoke through the contract to the specific implementation that
    is bound to the $contentGetter property.

--- src/FilterWPQuery.php
+++ src/FilterWPQuery.php
@@ -2,6 +2,7 @@
 
 namespace CalderaLearn\RestSearch;
 
+use CalderaLearn\RestSearch\ContentGetter\ContentGetterContract;
 use stdClass;
 use WP_Post;
 
@@ -14,6 +15,13 @@ use WP_Post;
  */
 class FilterWPQuery implements FiltersPreWPQuery
 {
+	/**
+	 * Content Getter Implementation.
+	 *
+	 * @var ContentGetterContract
+	 */
+	protected static $contentGetter;
+
 	/**
 	 * Priority for filter
 	 *
@@ -82,26 +90,6 @@ class FilterWPQuery implements FiltersPreWPQuery
 	/** @inheritdoc */
 	public static function getPosts(): array
 	{
-		return static::generatePosts(4);
-	}
-
-	/**
-	 * Generates an array of mocked posts.
-	 *
-	 * @param int $quantity Number of posts to generate.
-	 *
-	 * @return array
-	 */
-	private static function generatePosts($quantity): array
-	{
-		$mockPosts = [];
-		for ($postNumber = 0; $postNumber < $quantity; $postNumber++) {
-			$post             = new WP_Post( new stdClass() );
-			$post->post_title = "Mock Post {$postNumber}";
-			$post->filter     = 'raw';
-			$mockPosts[]      = $post;
-		}
-
-		return $mockPosts;
+		return static::$contentGetter->getContent(4);
 	}
 }
