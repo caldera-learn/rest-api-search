a1639c4fdda94a2137a3081ccb40e2b0e000d18c
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-27 13:13:36 -0500

    Mocked in unit tests.
    The new FilterWPQuery requires an implementation for getting the posts.
    This commit mocks the interface and then loads it via the `init()` method.
    
    In addition, switched to using Mockery and Brain Monkey instead of a separate
    mocked stub.

--- Tests/Unit/FilterWPQueryTest.php
+++ Tests/Unit/FilterWPQueryTest.php
@@ -2,146 +2,212 @@
 
 namespace CalderaLearn\RestSearch\Tests\Unit;
 
-use CalderaLearn\RestSearch\Tests\Mock\FilterWPQuery;
+use Brain\Monkey;
+use CalderaLearn\RestSearch\FilterWPQuery;
+use CalderaLearn\RestSearch\ContentGetter\ContentGetterContract;
+use Mockery;
 
 /**
  * Class FilterWPQueryTest
  *
- * Unit tests for class that modifies WP_Queryt
+ * Unit tests for class that modifies WP_Query
  *
  * @package CalderaLearn\RestSearch\Tests\Unit
  */
 class FilterWPQueryTest extends TestCase
 {
-	/**
-	 * Test that the filter priority is 10
-	 *
-	 * @covers \CalderaLearn\RestSearch\FilterWPQuery::getFilterPriority()
-	 * @covers \CalderaLearn\RestSearch\FilterWPQuery::$filterPriority
-	 */
-	public function testGetPriority()
-	{
-		$this->assertEquals(10, FilterWPQuery::getFilterPriority());
-	}
+    /**
+     * Array of mocked posts.
+     *
+     * @var array
+     */
+    protected $mockedPosts;
 
-	/**
-	 * Test that the getPosts method return an array
-	 *
-	 * @covers \CalderaLearn\RestSearch\FilterWPQuery::getPosts()
-	 */
-	public function testGetPosts()
-	{
-		//Get the mock posts
-		$results = FilterWPQuery::getPosts();
-		//Make sure results are an array
-		$this->assertTrue(is_array($results));
-	}
+    /**
+     * Prepares the test environment before each test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->mockedPosts = [];
+    }
 
-	/**
-	 * Test that the getPosts method return of WP_Posts
-	 *
-	 * @covers \CalderaLearn\RestSearch\FilterWPQuery::getPosts()
-	 */
-	public function testGetPostsArePosts()
-	{
-		//Get the mock posts
-		$results = FilterWPQuery::getPosts();
-		$this->assertFalse(empty($results));
-		//Make sure results are an array of WP_Posts
-		$looped = false;
-		foreach ($results as $result) {
-			$looped = true;
-			//Make sure all results are WP_Posts
-			$this->assertTrue(is_a($result, '\WP_Post'), get_class($result));
-		}
-		//Make sure loop ran
-		$this->assertTrue($looped);
-	}
+    /**
+     * Test that the filter priority is 10
+     *
+     * @covers \CalderaLearn\RestSearch\FilterWPQuery::getFilterPriority()
+     * @covers \CalderaLearn\RestSearch\FilterWPQuery::$filterPriority
+     */
+    public function testGetPriority()
+    {
+        $this->assertEquals(10, FilterWPQuery::getFilterPriority());
+    }
 
-	/**
-	 * Test that our mock returns true for should filter
-	 *
-	 * @covers FilterWPQuery::shouldFilter()
-	 * @covers FilterWPQueryTest::testWithNull()
-	 */
-	public function testShouldFilter()
-	{
-		$this->assertTrue(FilterWPQuery::shouldFilter(null));
-	}
+    /**
+     * Test that the getPosts method return an array
+     *
+     * @covers \CalderaLearn\RestSearch\FilterWPQuery::getPosts()
+     */
+    public function testGetPosts()
+    {
+        // Set up the Content Getter mock.
+        $postsGeneratorMock = Mockery::mock(ContentGetterContract::class);
+        $postsGeneratorMock->shouldReceive('getContent')->andReturn([]);
+        FilterWPQuery::init($postsGeneratorMock);
 
-	/**
-	 * Test the result data is consistent
-	 *
-	 * @covers \CalderaLearn\RestSearch\FilterWPQuery::filterPreQuery()
-	 */
-	public function testCallbackWithNull()
-	{
-		//Use the mock data we have in our mock class as the expected values
-		$expected = FilterWPQuery::getPosts();
+        //Get the mock posts
+        $results = FilterWPQuery::getPosts();
+        //Make sure results are an array
+        $this->assertTrue(is_array($results));
+    }
 
-		//Get the results from the callback
-		$results  = FilterWPQuery::filterPreQuery(null);
+    /**
+     * Test that the getPosts method return of WP_Posts
+     *
+     * @covers \CalderaLearn\RestSearch\FilterWPQuery::getPosts()
+     */
+    public function testGetPostsArePosts()
+    {
+        // Set up the mocks.
+        $postsGeneratorMock = Mockery::mock(ContentGetterContract::class);
+        FilterWPQuery::init($postsGeneratorMock);
+        $postsGeneratorMock->shouldReceive('getContent')
+            ->once()
+            ->andReturnUsing([$this, 'generateMockedPosts']);
 
-		//Make sure results are an array
-		$this->assertTrue(is_array($results));
+        //Get the mock posts
+        $results = FilterWPQuery::getPosts();
+        $this->assertFalse(empty($results));
+        //Make sure results are an array of WP_Posts
+        $looped = false;
+        foreach ($results as $result) {
+            $looped = true;
+            //Make sure all results are WP_Posts
+            $this->assertTrue(is_a($result, '\WP_Post'), get_class($result));
+        }
+        //Make sure loop ran
+        $this->assertTrue($looped);
+    }
 
-		//Make sure the two arrays are the same size
-		$this->assertCount(count($expected), $results);
+    /**
+     * Test that our mock returns true for should filter
+     *
+     * @covers FilterWPQuery::shouldFilter()
+     * @covers FilterWPQueryTest::testWithNull()
+     */
+    public function testShouldFilter()
+    {
+        Monkey\Functions\expect('did_action')
+            ->once()
+            ->with('rest_api_init')
+            ->andReturn(1);
+        $this->assertTrue(FilterWPQuery::shouldFilter(null));
+    }
 
-		/** These arrays are not the same, compare the meaning of the contents */
+    /**
+     * Test the result data is consistent
+     *
+     * @covers \CalderaLearn\RestSearch\FilterWPQuery::filterPreQuery()
+     */
+    public function testCallbackWithNull()
+    {
+        // Set up the mocks.
+        Monkey\Functions\expect('did_action')
+            ->once()
+            ->with('rest_api_init')
+            ->andReturn(1);
+        $postsGeneratorMock = Mockery::mock(ContentGetterContract::class);
+        FilterWPQuery::init($postsGeneratorMock);
+        $postsGeneratorMock->shouldReceive('getContent')
+            ->once()
+            ->andReturnUsing([$this, 'generateMockedPosts']);
 
-		//Used to make sure this loop of tests ran
-		$looped = false;
-		/**
-		 * Loop through expected, comparing to eactual results
-		 * @var int $i
-		 * @var  \WP_Post $post
-		 */
-		foreach ($expected as $i => $post) {
-			$looped = true;
-			//Test that the mock and resulting post titles are the same
-			$this->assertSame($post->post_title, $results[$i]->post_title);
-		}
-		//Make sure loop ran
-		$this->assertTrue($looped);
-	}
+        //Get the results from the callback
+        $results = FilterWPQuery::filterPreQuery(null);
 
-	/**
-	 * Test the result data is not changed, when passed an array
-	 *
-	 * * @covers \CalderaLearn\RestSearch\FilterWPQuery::filterPreQuery()
-	 */
-	public function testCallbackWithArray()
-	{
-		//Create 1 mock posts
-		$post = new \WP_Post((new \stdClass()));
-		$post->post_title = 'The title of this post is post';
-		$expected = [ $post ];
+        //Make sure results are an array
+        $this->assertTrue(is_array($results));
 
-		//Get the results from the callback
-		$results  = FilterWPQuery::filterPreQuery($expected);
+        //Make sure the two arrays are the same size
+        $this->assertCount(count($this->mockedPosts), $results);
 
-		//Make sure results are an array
-		$this->assertTrue(is_array($results));
+        /** These arrays are not the same, compare the meaning of the contents */
 
-		//Make sure the two arrays are the same size
-		$this->assertCount(count($expected), $results);
+        //Used to make sure this loop of tests ran
+        $looped = false;
+        /**
+         * Loop through expected, comparing to actual results
+         * @var int $i
+         * @var  \WP_Post $post
+         */
+        foreach ($this->mockedPosts as $index => $post) {
+            $looped = true;
+            //Test that the mock and resulting post titles are the same
+            $this->assertSame($post->post_title, $results[$index]->post_title);
+        }
+        //Make sure loop ran
+        $this->assertTrue($looped);
+    }
 
-		/** These arrays are not the same, compare the meaning of the contents */
+    /**
+     * Test the result data is not changed, when passed an array
+     *
+     * * @covers \CalderaLearn\RestSearch\FilterWPQuery::filterPreQuery()
+     */
+    public function testCallbackWithArray()
+    {
+        // Set up the mocks.
+        Monkey\Functions\expect('did_action')->with('rest_api_init')->never();
+        $postsGeneratorMock = Mockery::mock(ContentGetterContract::class);
+        FilterWPQuery::init($postsGeneratorMock);
+        $postsGeneratorMock->shouldNotReceive('getContent');
 
-		//Used to make sure this loop of tests ran
-		$looped = false;
-		/**
-		 * Loop through expected, comparing to eactual results
-		 * @var int $i
-		 * @var  \WP_Post $post
-		 */
-		foreach ($expected as $i => $post) {
-			$looped = true;
-			//Test that the mock and resulting post titles are the same
-			$this->assertSame($post->post_title, $results[$i]->post_title);
-		}
-		//Make sure loop ran
-		$this->assertTrue($looped);
-	}
+        //Create 1 mock posts
+        $expected = $this->generateMockedPosts(1);
+
+        //Get the results from the callback
+        $results = FilterWPQuery::filterPreQuery($expected);
+
+        //Make sure results are an array
+        $this->assertTrue(is_array($results));
+
+        //Make sure the two arrays are the same size
+        $this->assertCount(count($expected), $results);
+
+        /** These arrays are not the same, compare the meaning of the contents */
+
+        //Used to make sure this loop of tests ran
+        $looped = false;
+        /**
+         * Loop through expected, comparing to eactual results
+         * @var int $i
+         * @var  \WP_Post $post
+         */
+        foreach ($expected as $index => $post) {
+            $looped = true;
+            //Test that the mock and resulting post titles are the same
+            $this->assertSame($post->post_title, $results[$index]->post_title);
+        }
+        //Make sure loop ran
+        $this->assertTrue($looped);
+    }
+
+    /**
+     * Generates mocked posts for our tests.
+     *
+     * @param int $quantity Number of mocked posts to generate.
+     *
+     * @return array
+     */
+    public function generateMockedPosts($quantity = 4): array
+    {
+        $this->mockedPosts = [];
+        for ($postNumber = 0; $postNumber < $quantity; $postNumber++) {
+            $post                = Mockery::mock('WP_Post');
+            $post->post_title    = "Mock Post {$postNumber}";
+            $post->filter        = 'raw';
+            $this->mockedPosts[] = $post;
+        }
+        return $this->mockedPosts;
+    }
 }
